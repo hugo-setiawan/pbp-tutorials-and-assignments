54c590bad2176f27759d26d42cae442b18bf35fd
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-06 23:29:35 +0700

    Link Wishlist view and BarangWishlist model

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from wishlist.models import BarangWishlist
 
 # show_wishlist view as defined in tutorial document
 def show_wishlist(request):
-    return render(request, "wishlist.html")
+    # get data from BarangWishlist model
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Hugo'
+    }
+    return render(request, "wishlist.html", context)
