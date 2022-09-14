c069e91d7d86b0ff392b5b9428a958a534bea622
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-15 00:03:21 +0700

    Implement wishlist data delivery
    Functions were implemented to deliver data in XML and JSON

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, data_xml_wishlist, data_json_wishlist, data_json_wishlist_by_id, data_xml_wishlist_by_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', data_xml_wishlist, name='data_xml_wishlist'),
+    path('xml/<int:id>', data_xml_wishlist_by_id, name='data_xml_wishlist_by_id'),
+    path('json/', data_json_wishlist, name='data_json_wishlist'),
+    path('json/<int:id>', data_json_wishlist_by_id, name='data_json_wishlist_by_id'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # show_wishlist view as defined in tutorial document
 def show_wishlist(request):
@@ -10,3 +12,20 @@ def show_wishlist(request):
         'nama': 'Hugo'
     }
     return render(request, "wishlist.html", context)
+
+def data_xml_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data_barang_wishlist), content_type="applicaton/xml")
+
+def data_xml_wishlist_by_id(request, id):
+    data_barang_wishlist_by_id = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data_barang_wishlist_by_id), content_type="application/xml")
+
+def data_json_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data_barang_wishlist), content_type="application/json")
+
+def data_json_wishlist_by_id(request, id):
+    data_barang_wishlist_by_id = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data_barang_wishlist_by_id), content_type="application/json")
+
